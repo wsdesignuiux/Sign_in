3fc305313cf94a3ff54fbbeb0bfec22db1747da5
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-20 11:13:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/sign_in/Sign_inActivity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.sign_in;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_inActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
